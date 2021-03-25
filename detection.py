eefe0a4428ab625e88399b002056759003839e90
keenua <roman.olshevskiy@gmail.com>
2021-03-25 17:11:40 +0100

    cleanup

--- detection.py
+++ detection.py
@@ -3,34 +3,68 @@ import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 
-SCREENSHOT_DIR = 'e:\\Games\\Steam\\Screenshots'
-OY,OX = (300, 600)
-H,W = (600, 900)
+def fix_rotation(img, cnt):
+    rect = cv2.minAreaRect(cnt)
 
-img = cv2.imread(f'{SCREENSHOT_DIR}/379430_20210322222336_1.png')
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
 
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
-img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+    width = int(rect[1][0])
+    height = int(rect[1][1])
 
-main_img = img
-  
-classifier = cv2.CascadeClassifier('data\\all\\cascade.xml') 
-found = classifier.detectMultiScale(main_img[OY:OY+H, OX:OX+W], minSize = (60, 60), maxSize = (100, 100)) 
+    src_pts = box.astype("float32")
+    dst_pts = np.array([[0, height-1],
+                        [0, 0],
+                        [width-1, 0],
+                        [width-1, height-1]], dtype="float32")
 
-amount_found = len(found) 
-  
-if amount_found != 0: 
-    for (x, y, width, height) in found: 
-        x1,y1,x2,y2=(x + OX, y + OY, x + OX + height, y + OY + width)
-        die = img[y1:y2, x1:x2]
+    M = cv2.getPerspectiveTransform(src_pts, dst_pts)
+    warped = cv2.warpPerspective(img, M, (width, height))
+    return warped
 
+def detect(img):
+    result = []
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower_gray = np.array([21, 6, 0], np.uint8)
+    upper_gray = np.array([124, 71, 255], np.uint8)
+    mask_gray = cv2.inRange(hsv, lower_gray, upper_gray)
+
+    blur = cv2.bitwise_and(img, img, mask = mask_gray)
+    blur = cv2.cvtColor(blur,cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV)[1]
+
+    contours = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)[1]
+
+    imgcont = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
+
+    for c in contours:
+        area = cv2.contourArea(c)
+        cv2.drawContours(imgcont, [c], 0, (0,255,0), 1)
+
+        if 1800 < area < 5000:
+            res = fix_rotation(img, c)
+            rect = cv2.boundingRect(c)
+            result.append((rect, res))
+
+    return result
+
+if __name__ == '__main__':
+    SCREENSHOT_DIR = 'e:\\Games\\Steam\\Screenshots'
+    OY,OX = (200, 600)
+    H,W = (600, 900)
+
+    img = cv2.imread(f'{SCREENSHOT_DIR}/379430_20210322222253_1.png')
+    img = img[OY:OY+H, OX:OX+W]
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+
+    for (x,y,w,h), die in detect(img):
         res = recognize(die)
         print(res)
 
         plt.subplot(1, 1, 1) 
-        plt.imshow(img_rgb[y1:y2, x1:x2]) 
+        plt.imshow(img_rgb[y:y+h,x:x+w]) 
         plt.show()
 
-
-          
 
--- sample_collector.py
+++ sample_collector.py
@@ -1,14 +1,16 @@
 import time
+from os import mkdir, path
 from typing import *
+from uuid import uuid4
 
 import cv2
 import numpy as np
 
-from getkeys import key_check
+from detection import detect
+from getkeys import new_keys
 from grabscreen import grab_screen
 from recognition import recognize
-from uuid import uuid4
-from os import path
+
 
 def text(img: np.ndarray, text: str, pos: Tuple[int, int]):
     font                   = cv2.FONT_HERSHEY_SIMPLEX
@@ -26,31 +28,27 @@ def text(img: np.ndarray, text: str, pos: Tuple[int, int]):
 
 paused_status = False
 
-classifier = cv2.CascadeClassifier('data\\all\\cascade.xml') 
 save_to = 'e:\\Work\\Projects\\farkle\\train\\under\\'
-save_bg_to = 'e:\\Work\\Projects\\farkle\\train\\under\\bg'
 
 dice = []
-bg = None
+all = []
 
 while True:
-    pressed = key_check()
+    pressed, all = new_keys(all)
 
     if 'Y' in pressed: 
         paused_status = not(paused_status)
 
     if 'E' in pressed or 'Q' in pressed or 'F' in pressed:
-        for _, _, _, die in dice:
-            file = path.join(save_to, f'{uuid4()}.png')
+        for value, _, _, die in dice:
+            v_dir = path.join(save_to, str(value))
+            if not path.isdir(v_dir):
+                mkdir(v_dir)
+            file = path.join(v_dir, f'{uuid4()}.png')
             cv2.imwrite(file, die)
 
         print(f'{len(dice)} images saved!')
         continue
-
-    if bg is not None and 'B' in pressed:
-        file = path.join(save_bg_to, f'{uuid4()}.png')
-        cv2.imwrite(file, bg)
-        print(f'background image saved!')
 
     if paused_status: 
         print("Paused... (y)")
@@ -60,21 +58,16 @@ while True:
     img = grab_screen((600, 200, 1500, 800))
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGRA)
 
-    found = classifier.detectMultiScale(img, minSize = (75, 75), maxSize = (95, 95)) 
-
     dice = []
-    bg = img.copy()
-    for (x, y, width, height) in found:
-        die = img[y:y+width,x:x+height].copy()
-        value = recognize(die)
+    for (x, y, width, height), die  in detect(img):
+        cv2.rectangle(img, (x, y), (x + width, y + height), (0, 255, 0), 5) 
 
-        if value != 0:
-            cv2.rectangle(img, (x, y), (x + height, y + width), (0, 255, 0), 5) 
-            bg[y:y+width, x:x+height] = np.zeros((width, height, 4))
-            dice.append((recognize(die), x, y, die))
+        value = recognize(die)
+        dice.append((recognize(die), x, y, die))
     
     for (value, x, y, _) in dice:
-        text(img, str(value), (x, y))
+        if value != 0:
+            text(img, str(value), (x, y))
 
     cv2.imshow('cv2', img)
     cv2.waitKey(1)
